bd273015609ed10395ba08ab7b64589396fb7a85
ace <ace@ADA-1.local>
2024-12-09 14:49:49 +1100

    Added watch target

---
+++ src/HelloBeacon/HelloBeaconScanner Watch App/HelloBeaconScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloBeaconScannerApp.swift
+//  HelloBeaconScanner Watch App
+//
+//  Created by ace on 9/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloBeaconScanner_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
